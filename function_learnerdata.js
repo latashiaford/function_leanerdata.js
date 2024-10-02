ac02f2c14e7c61bf36999bcfc0ae9545c536ed06
latashiaford <latashiatyron@gmail.com>
2024-10-02 14:04:46 -0700

    Add files via upload

---
+++ function_learnerdata.js
@@ -0,0 +1,78 @@
+// create a function to process learner data
+function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
+}
+//that accepts these values as parameters, in the order listed//
+(CourseInfo, AssignmentGroup, [LearnerSubmission])
+//and returns formatted result,an array of objects as described above.//
+return function processLearnerSubmissions(learnerSubmissions) {
+    const results = [ courseInfo, assignmentGroup, learnerSubmissions) {
+    }];
+    return function processLearnerSubmissions(learnerSubmissions) {
+        const results = [];
+        return function processLearnerSubmissions(learnerSubmissions) {
+            const results = [];
+
+console.log(result)
+
+// Example//
+const courseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+  };
+  const assignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      }
+    ]
+  };
+  const learnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    }
+  ];
+  const result = getLearnerData(courseInfo, assignmentGroup, learnerSubmissions);
+  console.log(result);
+
+// Validate input data
+  if (courseID !== number) {
+   console.log ("this is not an course ID number");
+
+  }
+  if (AssignmentGroup !==course_id) {
+    console.log ("Error message the input was invalid");
+  
+// Process each learner's submissions
+const learners = [""];
+
+learners.forEach((learner, index) => {
+  console.log(125,125,118,132);
+});
+    
+  
